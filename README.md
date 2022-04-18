81c76cc65405703dccfb66cedf053d6d03b93395
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-04-18 14:32:44 +0200

    temp

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # My Waver Project
 
 ```
-contract address: 0x54b8fE53D2718d8c72F863D2e1C8820A673c225D
+contract address: 0x6AD2780fF73d956F5e8653c2DdB86dd782a57dD0
 ```
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,40 @@
 import type { NextPage } from "next";
 import { useWeb3React } from "@web3-react/core";
 import { injected, walletconnect } from "../connectors/connectors";
-import { Chip, Container, Fab, Stack, Typography } from "@mui/material";
+import {
+  Chip,
+  Container,
+  Fab,
+  Stack,
+  Typography,
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+} from "@mui/material";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import { useEffect, useState } from "react";
 import WalletModal from "../components/WalletModal";
 import { ethers } from "ethers";
 
+const IContract = require("../../artifacts/contracts/Waver.sol/Waver.json");
+const CHAIN_ID = "0x3";
+const contractAddress = "0x6AD2780fF73d956F5e8653c2DdB86dd782a57dD0";
+const abi = IContract.abi;
+
 const Home: NextPage = () => {
-  const { activate, active, account, connector, error } = useWeb3React();
+  const { activate, active, account, connector, error, library } =
+    useWeb3React();
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const [wavers, setWavers] = useState([]);
+  const [contract, setContract] = useState(null);
 
   useEffect(() => {
+    const init = async () => {
+      setUp();
+    };
+
     injected
       .isAuthorized()
       .then((isAuthorized) => {
@@ -26,28 +49,29 @@ const Home: NextPage = () => {
     if (localStorage.getItem("walletconnect")) {
       activate(walletconnect);
     }
+
+    if (active) {
+      init();
+    }
   }, [active, error, activate]);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   const handleInjected = async () => {
-    const chainId = 3;
-
     try {
       await activate(injected, undefined, true);
     } catch (e) {
       console.log(e);
     }
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-    const network = await provider.getNetwork();
+    const chainId = await connector?.getChainId();
 
-    if (network.chainId !== chainId) {
+    if (chainId !== CHAIN_ID) {
       try {
         await window.ethereum.request({
           method: "wallet_switchEthereumChain",
-          params: [{ chainId: ethers.utils.hexValue(chainId) }],
+          params: [{ chainId: CHAIN_ID as string }],
         });
       } catch (err) {
         console.log(err);
@@ -58,13 +82,45 @@ const Home: NextPage = () => {
   };
 
   const disconnect = async () => {
-    // localStorage.removeItem("walletconnect");
-    // connector?.getProvider().then((res) => console.log(res));
     try {
-      console.log(connector);
+      connector?.deactivate();
+      localStorage.removeItem("walletconnect");
     } catch (e) {
       console.log(e);
     }
+  };
+
+  const setUp = async () => {
+    const provider = new ethers.providers.Web3Provider(library.provider, "any");
+    const signer = await provider.getSigner();
+    const wavePortalContract = new ethers.Contract(
+      contractAddress,
+      abi,
+      signer
+    );
+    setContract(wavePortalContract);
+  };
+
+  const getWaversCount = async () => {
+    try {
+      let count = await contract.getTotalWavers();
+      console.log("Retrieved total wave count...", count.toNumber());
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const wave = async () => {
+    if (contract) {
+      let tx = await contract.wave(value);
+      tx.wait();
+      getAllWavers();
+    }
+  };
+
+  const getAllWavers = async () => {
+    const wavers = await contract.getAllWaves();
+    setWavers(wavers);
   };
 
   return (
@@ -97,7 +153,45 @@ const Home: NextPage = () => {
         <Chip color="primary" label="styled-components" onClick={() => {}} />
       </Stack>
 
-      <button onClick={() => void connector?.deactivate()}>Disconnect</button>
+      <FormControl sx={{ width: "100%" }}>
+        <InputLabel htmlFor="my-input">Text</InputLabel>
+        <Input
+          id="my-input"
+          aria-describedby="my-helper-text"
+          onChange={({ target }) => setValue(target.value)}
+          value={value}
+        />
+        <Button onClick={wave}>Send</Button>
+      </FormControl>
+
+      <Button
+        onClick={() => {
+          console.log(getWaversCount());
+        }}
+      >
+        Log wavers count
+      </Button>
+
+      <Button
+        onClick={() => {
+          console.log(getAllWavers());
+        }}
+      >
+        Log all wavers
+      </Button>
+
+      {wavers.map((waver) => {
+        return (
+          <>
+            <Typography variant="body1" sx={{ mb: 1 }}>
+              {waver.from}
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 1 }}>
+              {waver.message}
+            </Typography>
+          </>
+        );
+      })}
 
       <WalletModal
         open={open}
